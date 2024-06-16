542c633030ec7e39b59a53b009c7e9ceea9c143d
denniskobla <102664240+denniskobla@users.noreply.github.com>
2024-06-16 01:11:47 +0000

    IMovable
    Third class( Interfaces)

---
+++ IMovable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car();
+        car.Move();
+
+        IMovable bicycle = new Bicycle();
+        bicycle.Move();
+
+        Console.ReadLine();
+    }
+}
