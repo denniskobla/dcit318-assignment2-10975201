968f8461164561c42ceb839aa7e7f8bc11c7ff1d
denniskobla <102664240+denniskobla@users.noreply.github.com>
2024-06-16 01:00:15 +0000

    Animals
    First tasks (inheritance and method overriding

---
+++ Animals/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.MakeSound();
+
+        Dog dog = new Dog();
+        dog.MakeSound();
+
+        Cat cat = new Cat();
+        cat.MakeSound();
+
+        Console.ReadLine();
+    }
+}
